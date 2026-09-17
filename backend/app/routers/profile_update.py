c26c12fd44a0f8f18ba93f0f337f5cac6c8cc8a1
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-09-18 00:29:04 +0300

    make the user able to change his password (backend api)

--- backend/app/routers/profile_update.py
+++ backend/app/routers/profile_update.py
@@ -1,9 +1,9 @@
 from fastapi import Depends , status , APIRouter 
 from app.helpers.get_current_user import get_current_user 
-from app.services.userProfile import update_profile_service 
+from app.services.userProfile import update_profile_service , change_password_service
 from app.database.db import get_db 
 from sqlalchemy.orm import Session 
-from app.schemas.user import UserResponse, UserUpdate
+from app.schemas.user import UserResponse, UserUpdate  , ChangePasswordRequest
 from app.models.user import User
 
 
@@ -20,3 +20,11 @@ def update_profile(
 ):
     user = update_profile_service(user_data=user_data , current_user=current_user,db=db)
     return user
+
+@router.patch("/chage-password")
+def change_password(
+    password_data : ChangePasswordRequest,
+    current_user : User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    return change_password_service(current_user=current_user , password_data=password_data , db=db)
--- backend/app/services/userProfile.py
+++ backend/app/services/userProfile.py
@@ -1,7 +1,8 @@
 from app.models.user import User 
-from app.schemas.user import UserUpdate  , ChangePasswordRequest 
+from app.schemas.user import UserUpdate , ChangePasswordRequest 
 from sqlalchemy.orm import Session 
 from fastapi import HTTPException , status
+from app.core.security import password_hash
 
 def update_profile_service(
     user_data: UserUpdate,
@@ -40,5 +41,25 @@ def update_profile_service(
 
     return current_user
     
-def update_password(current_user:User , user_data:ChangePasswordRequest , db: Session):
-    pass
+def change_password_service(current_user:User , password_data:ChangePasswordRequest , db: Session):
+    if not password_hash.verify(
+        password_data.current_password , current_user.password_hash
+    ):
+        raise HTTPException (
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Current password is incorrect"
+        )
+
+    if password_hash.verify(
+        password_data.new_password , current_user.password_hash
+    ):
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail="New password must be different from current password"
+        )
+    current_user.password_hash = password_hash.hash(
+        password_data.new_password
+    )
+
+    db.commit()
+    return {"message":"Password changed successfully"}
